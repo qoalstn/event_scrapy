06acd3feeab246ee5b8228642b857952a15bfcd2
ksk0622 <mousson2020@gmail.com>
2023-05-22 16:49:26 +0900

    feat : 다음페이지로 넘기면서 데이터 가져오기

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ browser = webdriver.Chrome()
 browser.maximize_window()
 
 from bs4 import BeautifulSoup
-soup = BeautifulSoup(browser.page_source, "lxml")
+# soup = BeautifulSoup(browser.page_source, "lxml")
 
 import re
 import time
@@ -17,10 +17,6 @@ url = 'http://gs25.gsretail.com/gscvs/ko/products/event-goods#;'
 res = browser.get(url)
 page_soup = BeautifulSoup(browser.page_source, 'html.parser')
 
-
-# # 마지막 페이지 구하기
-# last_page = browser.find_element(By.XPATH,'//*[@id="contents"]/div[2]/div[3]/div/div/div[1]/div/a[4]')
-# last_page.click()
 
 ## 스크롤
 # 현재 문서 높이를 가져와서 저장
@@ -47,4 +43,42 @@ p = re.compile('(?<=movePage\()(.*?)(?=\))')
 text =page_soup.select('a.next2')[0]
 rs = p.findall(str(text))
 
-print(int(rs[0]))
+last_page = int(rs[0])
+print(last_page)
+
+for i in range(1, 4) :
+    while True:
+        # 스크롤을 가장 아래로 내림
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+
+        # 페이지 로딩 대기
+        time.sleep(2)
+
+        # 현재 문서 높이를 가져와서 저장
+        curr_height = browser.execute_script("return document.body.scrollHeight")
+        if curr_height == prev_height:
+            break
+
+        prev_height = curr_height
+
+        print("스크롤 완료")
+    page_soup = BeautifulSoup(browser.page_source, 'html.parser')
+    items = page_soup.find_all("div", attrs={"class":"prod_box"})
+
+    print('prod_box len : ', len(items))
+    
+    for item in items:
+        title = item.find("p", attrs={"class":"tit"}).get_text()
+        price = item.find("span", attrs={"class":"cost"})
+        img = item.find("img")['src']
+
+        print(f"제목 : {title}")
+        # print(f"금액 : {price}")
+        # print(f"이미지 : {img}")
+
+    next_page = browser.find_element(By.XPATH,'//*[@id="contents"]/div[2]/div[3]/div/div/div[1]/div/a[3]')
+    next_page.click()
+
+
+# last_page = browser.find_element(By.XPATH,'//*[@id="contents"]/div[2]/div[3]/div/div/div[1]/div/a[4]')
+# last_page.click()
