7d356a468c220a237b22abb5aaff609525574ea1
ksk0622 <mousson2020@gmail.com>
2023-05-30 17:30:30 +0900

    refactor : 이벤트 목록 중복 함수 제거

--- store/urls.py
+++ store/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.hello, name='hello'),
-    path('list/', views.showEvent, name='event_list'),
-    path('regist/gs', views.saveEventGS, name='event_regist_gs'),
-    path('regist/cu', views.saveEventCU, name='event_regist_cu'),
+    path('', views.hello, name='hello'),
+    path('list/<str:name>/',
+         views.showEvent, name='event_list'),
+    # path('regist/gs', views.saveEventGS, name='event_regist_gs'),
+    # path('regist/cu', views.saveEventCU, name='event_regist_cu'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import ItemGs25
+from .models import ItemGs25, ItemCu
 
 
 def hello(request):
@@ -9,49 +9,32 @@ def hello(request):
 def saveEvent(request):
     print('request :: ', request)
 
-    # template_values = { 
-    #     'name' : '방울토마토',
-    #     'price' : 2000,
-    #     'img' : 'https://image.woodongs.com/imgsvr/item/GD_8809168367350_001.jpg'
-    # } 
-    return render(request, 'store/event.html')
-
-def showEventGS(request):
-    print('request >>',request.GET.get('name'))
-
-    if(request.method == 'GET'):
+    if (request.method == 'GET'):
         name = request.GET.get('name')
         price = request.GET.get('price')
         img = request.GET.get('img')
 
-        data = ItemGs25(item_idx='G00001',name=name, price=price, img=img)
+        data = ItemGs25(item_idx='G00001', name=name, price=price, img=img)
         data.save()
 
-        new_data = ItemGs25.objects.all()
-
-        print('new_data >> ', new_data)
-
-
-        # data = ItemGs25(template_values)
-        # data.save()
     return render(request, 'store/event.html')
 
-def showEventCU(request):
-    print('request >>',request.GET.get('name'))
 
-    if(request.method == 'GET'):
-        name = request.GET.get('name')
-        price = request.GET.get('price')
-        img = request.GET.get('img')
+def showEvent(request, name):
+    print('request :: ', request)
+    print('storeName : ', name)
 
-        data = ItemGs25(item_idx='G00001',name=name, price=price, img=img)
-        data.save()
+    items = []
+    if (request.method == 'GET'):
+        if (name == 'gs'):
+            items.append(getItems(ItemGs25))
+        if (name == 'cu'):
+            items.append(getItems(ItemCu))
 
-        new_data = ItemGs25.objects.all()
+    print('items : ', items)
 
-        print('new_data >> ', new_data)
+    return render(request, 'store/event-list.html')
 
 
-        # data = ItemGs25(template_values)
-        # data.save()
-    return render(request, 'store/event.html')
+def getItems(model):
+    return model.objects.all()
