f04309a5d0154c57a60ec554c3898684b7819963
ksk0622 <mousson2020@gmail.com>
2023-06-13 10:07:37 +0900

    fix

---
+++ store/database/gs_repository.py
@@ -0,0 +1,45 @@
+import sqlite3
+import datetime
+
+def deleteAllDatas():
+    # 데이터베이스 연결
+    conn = sqlite3.connect('database.db',check_same_thread=False)
+    cursor = conn.cursor()
+
+    # 모든 레코드 삭제    
+    cursor.execute('''DELETE FROM item_gs25''')
+    conn.commit()
+    conn.close()
+
+
+
+## 크롤링 데이터 저장
+def saveCrawlData( item_idx, name, price, img):
+    # 데이터베이스 연결
+    conn = sqlite3.connect('database.db',check_same_thread=False)
+    cursor = conn.cursor()
+
+    now = datetime.datetime.now() # 현재 날짜와 시간
+    last_day = datetime.datetime(now.year, now.month + 1, 1) - datetime.timedelta(days=1) # 이번 달의 마지막 날
+
+    cursor.execute('''INSERT INTO item_gs25 (item_idx, name, price, img, created_at ,updated_at ,expired_at) VALUES (?, ?, ?, ?, ?, ?, ?)''', (item_idx, name, price, img, now, now,last_day))
+    conn.commit()
+
+    conn.close()
+
+## 크롤링한 모든 데이터
+def selectAllGS():
+    # 데이터베이스 연결
+    conn = sqlite3.connect('database.db',check_same_thread=False)
+    cursor = conn.cursor()
+
+    cursor.execute('''SELECT * FROM item_gs25''')
+    rows =  cursor.fetchall()
+
+    list = []
+    for row in rows:
+        list.append({'item_idx':row[0],'title':row[1],'price':row[2],'img':row[3]})    
+
+    conn.close()
+
+    return list
