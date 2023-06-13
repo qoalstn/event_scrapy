51c4e7eff78e1d12e07c8967ae167c4073b0b738
dahye <tizlspffl@naver.com>
2023-06-13 10:12:06 +0900

    fis

--- store/services/cu_crawl.py
+++ store/services/cu_crawl.py
@@ -14,42 +14,43 @@ headers = {
 
 }
 
+def cu_crawl():
+    sale_product = []
+    for i in range(1, 2):
+        params = {
+            "pageIndex": i,
+            "listType": 1,
+            "searchCondition": "",
+            "user_id": ""
+        }
+    res = requests.post(
+        "https://cu.bgfretail.com/event/plusAjax.do", headers=headers, data=params)
+    soup = BeautifulSoup(res.text, "html.parser")
+    prod_badge = soup.find_all('div', "badge")
+    for b in prod_badge:
+        badges = b.find_all(["span"])
+        for badge in badges:
+            sale_badge = badge.text
+    prod_all = soup.find_all("div", {'class': 'prod_wrap'})
+    for i in prod_all:
+        prod_name = i.find_all(['p'])
+        for name in prod_name:
+            sale_name = name.text
+        prod_price = i.find_all(['strong'])
+        for price in prod_price:
+            sale_price = price.text+"원"
+        prod_img = i.find_all("img")
 
-for i in range(1, 2):
-    params = {
-        "pageIndex": i,
-        "listType": 1,
-        "searchCondition": "",
-        "user_id": ""
-    }
-res = requests.post(
-    "https://cu.bgfretail.com/event/plusAjax.do", headers=headers, data=params)
-soup = BeautifulSoup(res.text, "html.parser")
-prod_badge = soup.find_all('div', "badge")
-for b in prod_badge:
-    badges = b.find_all(["span"])
-    for badge in badges:
-        sale_badge = badge.text
-prod_all = soup.find_all("div", {'class': 'prod_wrap'})
-for i in prod_all:
-    prod_name = i.find_all(['p'])
-    for name in prod_name:
-        sale_name = name.text
-    prod_price = i.find_all(['strong'])
-    for price in prod_price:
-        sale_price = price.text+"원"
-    prod_img = i.find_all("img")
-
-    for img in prod_img:
-        img_src = "http" not in img['src']
-        if img_src:
-            sale_src = "https:"+img['src']
-        else:
-            sale_src = img['src']
-    sale_product = {'name': sale_name, 'price': sale_price,
-                    'img': sale_src, 'plus': sale_badge}
-    # print(sale_product)
-
-    # print(json.dumps(sale_product, ensure_ascii=False, indent=2))
-    with open('cu_example.json', 'w', encoding='utf-8') as f:
-        json.dump(sale_product, f, ensure_ascii=False, indent="\t")
+        for img in prod_img:
+            img_src = "http" not in img['src']
+            if img_src:
+                sale_src = "https:"+img['src']
+            else:
+                sale_src = img['src']
+        sale_product.append({'name': sale_name, 'price': sale_price,
+                        'img': sale_src, 'plus': sale_badge})
+        
+        # print(json.dumps(sale_product, ensure_ascii=False, indent=2))
+    print(sale_product)   
+    
+    return sale_product   
