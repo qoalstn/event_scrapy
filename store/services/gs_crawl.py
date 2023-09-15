b15a2799eb62c6f335f155e5fc3c2b9f974fab85
qoalstn <mousson2020@gmail.com>
2023-09-15 14:00:15 +0900

    fix

--- store/services/gs_crawl.py
+++ store/services/gs_crawl.py
@@ -7,6 +7,11 @@ from selenium.webdriver.common.by import By
 # from selenium.webdriver.support.ui import WebDriverWait
 # from selenium.webdriver.support import expected_conditions as EC
 # from webdriver_manager.chrome import ChromeDriverManager
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
@@ -14,8 +19,7 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument("--single-process")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-browser = webdriver.Chrome(executable_path="C:/Users/seang/Downloads/chromedriver.exe",chrome_options=chrome_options)
-# browser = webdriver.Chrome(executable_path="/usr/bin/chromedriver",chrome_options=chrome_options)
+browser = webdriver.Chrome(executable_path=os.environ.get("CHROME_DRIVER_PATH"),chrome_options=chrome_options)
 
 browser.maximize_window()
 
